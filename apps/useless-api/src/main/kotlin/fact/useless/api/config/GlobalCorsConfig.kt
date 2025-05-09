2e8f22e3f77a94015b4de5549d169b24e5cc76b6
Victor Das <victordas@msn.com>
2025-05-09 16:50:45 +0200

    - enable cors for frontend development

---
+++ apps/useless-api/src/main/kotlin/fact/useless/api/config/GlobalCorsConfig.kt
@@ -0,0 +1,28 @@
+package fact.useless.api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.reactive.CorsConfigurationSource
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+@Configuration
+class GlobalCorsConfig {
+
+  @Bean
+  fun corsWebFilter(): CorsWebFilter {
+    val corsConfiguration = CorsConfiguration().apply {
+      addAllowedOrigin("http://localhost:4200") // Allow localhost:4200
+      addAllowedHeader("*") // Allow any header
+      addAllowedMethod("*") // Allow any HTTP method (GET, POST, etc.)
+    }
+
+    val source: CorsConfigurationSource = UrlBasedCorsConfigurationSource().apply {
+      registerCorsConfiguration("/**", corsConfiguration) // Apply CORS configuration to all endpoints
+    }
+
+    return CorsWebFilter(source)
+  }
+}
+
